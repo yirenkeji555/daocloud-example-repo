9879f8eb547911eca29beb099e02ab4f84b11d25
贵旦 <excel555@163.com>
2020-03-06 12:09:02 +0800

    new port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var server = http.createServer(router({
   }
 }))
 
-server.listen(parseInt(process.env.PORT) || 80, function() {
+server.listen(8090, function() {
   console.log('process.env.PORT', process.env.PORT)
   console.log('process.env.PORT', process.env)
   console.log('Docker DEMO with Node.js is running.')
